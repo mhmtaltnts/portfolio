553a2c664722d5642c885b7e18c2fc3bbc2fdcaa
mhmtaltnts <12mart1973@gmail.com>
2022-07-27 19:48:45 +0300

    fidanli-akşam

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,8 +4,8 @@ import ContactItem from "../../components/ContactItem/ContactItem"
 import { ThemeContext } from "../../context/ThemeContext"
 import "./Contact.scss"
 const title = {
-  en: ["My Portfolio", "My Work"],
-  tr: ["Portfolyom", "Çalışmalarım"]
+  en: ["Contact", "Contact"],
+  tr: ["İletişim", "İletişim"]
 }
 
 const contactitems = {
@@ -13,6 +13,11 @@ const contactitems = {
     ["fas fa-map-marker-alt", "Location", "Gebze/Kocaeli Türkiye"],
     ["fas fa-envelope", "Email", "12mart1973@gmail.com"],
     ["fas fa-user-graduate", "Mobile Number", "+905392882703"]
+  ],
+  tr: [
+    ["fas fa-map-marker-alt", "Adres", "Gebze/Kocaeli Türkiye"],
+    ["fas fa-envelope", "Eposta", "12mart1973@gmail.com"],
+    ["fas fa-user-graduate", "Cep Telefonu", "+905392882703"]
   ]
 }
 function Contact() {
@@ -31,28 +36,29 @@ function Contact() {
               ))}
             </div>
           </div>
-        </div>
-        <div className="right-contact">
-          <form action="" class="contact-form">
-            <div className="input-control i-c-2">
-              <input type="text" required placeholder="YOUR NAME" />
-              <input type="email" required placeholder="YOUR EMAIL" />
-            </div>
-            <div className="input-control">
-              <input type="text" required placeholder="ENTER SUBJECT" />
-            </div>
-            <div className="input-control">
-              <textarea name="" id="" cols="15" rows="8" placeholder="Message Here..."></textarea>
-            </div>
-            <div className="submit-btn">
-              <a href="#" class="main-btn">
-                <span className="btn-text">Download CV</span>
-                <span className="btn-icon">
-                  <i className="fas fa-download"></i>
-                </span>
-              </a>
-            </div>
-          </form>
+
+          <div className="right-contact">
+            <form action="" class="contact-form">
+              <div className="input-control i-c-2">
+                <input type="text" required placeholder="YOUR NAME" />
+                <input type="email" required placeholder="YOUR EMAIL" />
+              </div>
+              <div className="input-control">
+                <input type="text" required placeholder="ENTER SUBJECT" />
+              </div>
+              <div className="input-control">
+                <textarea name="" id="" cols="15" rows="8" placeholder="Message Here..."></textarea>
+              </div>
+              <div className="submit-btn">
+                <a href="#" class="main-btn">
+                  <span className="btn-text">Download CV</span>
+                  <span className="btn-icon">
+                    <i className="fas fa-download"></i>
+                  </span>
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </section>
